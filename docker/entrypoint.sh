d462f607acc4722fafee1c2c4a539f62529604a1
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2019-04-10 11:13:36 +0200

    docker added

---
+++ docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+export PGHOST=${IRIS_API_POSTGRESQL_SERVICE_HOST:-postgres}
+export PGUSER=${POSTGRES_USER:-postgres}
+export PGPASSWORD=$BLACKHOLE_DB_PASSWORD
+export PGDATABASE=${BLACKHOLE_POSTGRES_DB:-blackhole_production}
+
+yarn setupdb
+yarn production
